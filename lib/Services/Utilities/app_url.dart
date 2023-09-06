a914d3e028d291f959868247a24431acaef38bae
Ayush Aryan <ayusharyan1309@gmail.com>
2023-09-06 10:38:05 +0530

    Initial files

---
+++ lib/Services/Utilities/app_url.dart
@@ -0,0 +1,11 @@
+// all the API used in the app will be defined here
+ class AppUrl{
+  // base url
+  static const String baseUrl = 'https://disease.sh/v3/covid-19/';
+
+  // detching world covid states
+
+ static const String worldStatesApi = baseUrl + "all";
+ static const String countriesList = baseUrl + 'countries';
+
+ }
